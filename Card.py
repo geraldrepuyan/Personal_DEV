6c396afe106f86e8329966ee210eeea2c4424873
Gerald Repuyan <geraldrepuyan@geralds-air.myfiosgateway.com>
2022-05-24 00:26:42 -0400

    initial merge

---
+++ Card.py
@@ -0,0 +1,124 @@
+# The Card class definition
+class Card:
+    def __init__(self, suit, value, card_value):
+         
+        # Suit of the Card like Spades and Clubs
+        self.suit = suit
+ 
+        # Representing Value of the Card like A for Ace, K for King
+        self.value = value
+ 
+        # Score Value for the Card like 10 for King
+        self.card_value = card_value
+
+# Function to print the cards
+def display_cards(cards, hidden):
+         
+    s = ""
+    for card in cards:
+        s = s + "\t ________________"
+    if hidden:
+        s += "\t ________________"
+    print(s)
+ 
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|                |"    
+    print(s)
+ 
+    s = ""
+    for card in cards:
+        if card.value == '10':
+            s = s + "\t|  {}            |".format(card.value)
+        else:
+            s = s + "\t|  {}             |".format(card.value)  
+    if hidden:
+        s += "\t|                |"    
+    print(s)
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|      * *       |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|    *     *     |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|   *       *    |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|   *       *    |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|       {}        |".format(card.suit)
+    if hidden:
+        s += "\t|          *     |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|         *      |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|        *       |"
+    print(s)
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|                |"
+    print(s)
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|                |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        if card.value == '10':
+            s = s + "\t|            {}  |".format(card.value)
+        else:
+            s = s + "\t|            {}   |".format(card.value)
+    if hidden:
+        s += "\t|        *       |"        
+    print(s)    
+         
+    s = ""
+    for card in cards:
+        s = s + "\t|________________|"
+    if hidden:
+        s += "\t|________________|"
+    print(s)        
+ 
+    print()
+
+    
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Personal_DEV
-# Praeses Blackjack program
+# Praeses Blackjack program - Repuyan
 
-When running from a command line (BASH, ZSH), ensure you are running a version of Python 3.*
+*When running from a command line (BASH, ZSH), ensure you are running a version of Python 3 or higher.* 
 
-Navigate to directory of *.py files, execute the following command: 
+Navigate to directory of *.py files are staged. 
 
-$ python3 <filename>
+To run Blackjack program, execute the following command: 
+
+"$ python Blackjack.py"
+
+Follow all instructions as they populate on the command line. 
+
+ENJOY!
